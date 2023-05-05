0f52f0a6110e48f757bc40960255748b124abb57
Blake Taylor <sephnescence@gmail.com>
2023-05-05 16:24:23 +1000

    Another Minimum Absolute Difference in BST editorial solution and notes

--- TreesPlayground/src/main/java/MinimumAbsoluteDifferenceInBST/MinimumAbsoluteDifferenceInBST.java
+++ TreesPlayground/src/main/java/MinimumAbsoluteDifferenceInBST/MinimumAbsoluteDifferenceInBST.java
@@ -2,65 +2,33 @@ package MinimumAbsoluteDifferenceInBST;
 
 import Helpers.TreeNode;
 
-// This solution also beats 59% in runtime and 89% in memory usage (Though memory usage swings a lot apparently, but the highest I've seen was 98% so that is technically the best so far)
-// When submitting to leetcode, just be sure to rename this class to "Solution"
-// I'm not naming it Solution because the test files begin to go crazy if there are multiple packages with classes called "Solution"
-// Also, only copy from the next line down
+// Borrowing the editorial approach which beats 100%|92%
+// I see. This approach relies on hitting nulls, and has a reference to the previous node so that it can check the previous node instead of checking if it's a leaf node byt checking either side
 public class MinimumAbsoluteDifferenceInBST {
-    private int minimumDifference = Integer.MAX_VALUE;
-    public int getMinimumDifference(TreeNode root) {
-        // I have two tests running across this now...
-        // One - [979,3,2997,null,null,1254,null,null,2048] - the result in 275. abs(979 - 1254)
-        // Two - [979,3,2997,null,978,1254,null,null,2048] - the result in 1. abs(979 - 978)
-        // Three - [236,104,701,null,227,null,911] - the result is 9. abs(236, 227)
+    int minDifference = Integer.MAX_VALUE;
+    // Initially, it will be null.
+    TreeNode prevNode;
 
-        if (root == null) {
-            return 0;
-        }
-
-        // So how do we leverage the fact that this is a BST...?
-        // I think all we can do is not traverse left the value of the node didn't move the minimum difference?
-        _dfsTraverseTree(root);
-
-        return minimumDifference;
-    }
-
-    private void _dfsTraverseTree(TreeNode node) {
+    void inorderTraversal(TreeNode node) {
         if (node == null) {
             return;
         }
 
-        // Visit self
-        // Improving again, you just want the highest value from the left, and the lowest value from the right
-        // You only need to check the minimum difference after each of these return
-        if (node.left != null) {
-            int highestLeft = _getMaximumDifferenceRight(node.left);
-            minimumDifference = Math.min(minimumDifference, Math.abs(node.val - highestLeft));
-            _dfsTraverseTree(node.left);
+        inorderTraversal(node.left);
+        // Find the difference with the previous value if it is there.
+        if (prevNode != null) {
+            // minDifference = Math.min(minDifference, node.val - prevNode.val); // Interesting to note there's no abs here...
+            // This means my personal scenario hits a negative and wins cos it's the smallest
+            minDifference = Math.min(minDifference, Math.abs(node.val - prevNode.val)); // But then we're back to the 275 issue
+            // I see. It can't see the 978 because it can only look left, despite 978 being in a valid location
+            // I wonder if there's just an issue with the test data on leetcode?
         }
-
-        if (node.right != null) {
-            int lowestRight = _getMinimumDifferenceLeft(node.right);
-            minimumDifference = Math.min(minimumDifference, Math.abs(node.val - lowestRight));
-            _dfsTraverseTree(node.right);
-        }
+        prevNode = node;
+        inorderTraversal(node.right);
     }
 
-    private int _getMinimumDifferenceLeft(TreeNode node) {
-        if (node.left == null) {
-            return node.val;
-        }
-
-        // Keep visiting left
-        return _getMinimumDifferenceLeft(node.left);
-    }
-
-    private int _getMaximumDifferenceRight(TreeNode node) {
-        if (node.right == null) {
-            return node.val;
-        }
-
-        // Keep visiting right
-        return _getMaximumDifferenceRight(node.right);
+    int getMinimumDifference(TreeNode root) {
+        inorderTraversal(root);
+        return minDifference;
     }
 }
