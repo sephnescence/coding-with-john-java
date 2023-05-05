e3818585b5ddd40034aa2f75d406d4c3e8f1bbcf
Blake Taylor <sephnescence@gmail.com>
2023-05-05 15:43:51 +1000

    Minimum Absolute Difference in BST solved 58%|37%

--- TreesPlayground/src/main/java/MinimumAbsoluteDifferenceInBST/MinimumAbsoluteDifferenceInBST.java
+++ TreesPlayground/src/main/java/MinimumAbsoluteDifferenceInBST/MinimumAbsoluteDifferenceInBST.java
@@ -2,22 +2,24 @@ package MinimumAbsoluteDifferenceInBST;
 
 import Helpers.TreeNode;
 
-// This solution beats 19% in runtime and 46% in memory usage
+// This solution beats 58% in runtime and 37% in memory usage
 // When submitting to leetcode, just be sure to rename this class to "Solution"
 // I'm not naming it Solution because the test files begin to go crazy if there are multiple packages with classes called "Solution"
 // Also, only copy from the next line down
 public class MinimumAbsoluteDifferenceInBST {
     private int minimumDifference = Integer.MAX_VALUE;
     public int getMinimumDifference(TreeNode root) {
-        // For example, given [979,3,2997,null,null,1254,null,null,2048]
-        // the result in 275. abs(979 - 1254)
-        // Somehow, however, I get 794 all the time, which is 1254-2048
+        // I have two tests running across this now...
+        // One - [979,3,2997,null,null,1254,null,null,2048] - the result in 275. abs(979 - 1254)
+        // Two - [979,3,2997,null,978,1254,null,null,2048] - the result in 1. abs(979 - 978)
+        // Three - [236,104,701,null,227,null,911] - the result is 9. abs(236, 227)
 
         if (root == null) {
             return 0;
         }
 
-        // Brute forcing should be relatively easy. Traverse the whole tree for every tree and see where the min difference is
+        // So how do we leverage the fact that this is a BST...?
+        // I think all we can do is not traverse left the value of the node didn't move the minimum difference?
         _dfsTraverseTree(root);
 
         return minimumDifference;
@@ -47,10 +49,17 @@ public class MinimumAbsoluteDifferenceInBST {
         // Visit self
         minimumDifference = Math.min(minimumDifference, Math.abs(root.val - node.val));
 
-        // Visit left
-        _getMinimumDifference(root, node.left);
+        // for example, 236 is the root, and the left side's value is 104, and has a child on the right with a value of 227
+        // 104 might have moved the difference, but literally any other value on the right will tighten the gap, so maybe keep going right
+        // Likewise, when on the right, keep going left
 
-        // Visit right
-        _getMinimumDifference(root, node.right);
+        // I think this behaviour flips depending on the side you're on
+        if (node.val >= root.val) { // i.e. on the right
+            // Visit left conditionally
+            _getMinimumDifference(root, node.left);
+        } else { // i.e. on the left
+            // Visit right conditionally
+            _getMinimumDifference(root, node.right);
+        }
     }
 }
--- TreesPlayground/src/test/java/MinimumAbsoluteDifferenceInBST/MinimumAbsoluteDifferenceInBSTTest.java
+++ TreesPlayground/src/test/java/MinimumAbsoluteDifferenceInBST/MinimumAbsoluteDifferenceInBSTTest.java
@@ -8,13 +8,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MinimumAbsoluteDifferenceInBSTTest {
     @Test
-    void testTrue() {
-        assertTrue(true);
+    void testLeetCodeScenario() {
         MinimumAbsoluteDifferenceInBST solution = new MinimumAbsoluteDifferenceInBST();
         TreeNode root = GenerateTreeNodesFromString.fromIntegerArray(new Integer[]{979,3,2997,null,null,1254,null,null,2048});
 
         int result = solution.getMinimumDifference(root);
 
         assertEquals(275, result);
+    }
+
+    @Test
+    void testPersonalScenarioBasedOffLeetCode() {
+        MinimumAbsoluteDifferenceInBST solution = new MinimumAbsoluteDifferenceInBST();
+        TreeNode root = GenerateTreeNodesFromString.fromIntegerArray(new Integer[]{979,3,2997,null,978,1254,null,null,2048});
+
+        int result = solution.getMinimumDifference(root);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void testAnotherLeetCodeScenario() {
+        MinimumAbsoluteDifferenceInBST solution = new MinimumAbsoluteDifferenceInBST();
+        TreeNode root = GenerateTreeNodesFromString.fromIntegerArray(new Integer[]{236,104,701,null,227,null,911});
+
+        int result = solution.getMinimumDifference(root);
+
+        assertEquals(9, result);
     }
 }
